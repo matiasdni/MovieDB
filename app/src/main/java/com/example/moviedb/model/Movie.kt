fd3fd33f7dffd32d442fb2b862650b5d07268c84
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-09-25 08:48:51 +0300

    data class for movie

---
+++ app/src/main/java/com/example/moviedb/model/Movie.kt
@@ -0,0 +1,14 @@
+package com.example.moviedb.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    @SerializedName("poster_path")
+    val posterPath: String?,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("release_date")
+    val releaseDate: String,
+)

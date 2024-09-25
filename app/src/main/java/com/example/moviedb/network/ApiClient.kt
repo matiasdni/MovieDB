c55970b0288ec7747a41189e05b666b719f6ebde
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-09-25 08:55:10 +0300

    retrofit client object

---
+++ app/src/main/java/com/example/moviedb/network/ApiClient.kt
@@ -0,0 +1,26 @@
+package com.example.moviedb.network
+
+import com.example.moviedb.BuildConfig
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+    private const val API_KEY = BuildConfig.API_KEY
+
+    private val client by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor(API_KEY))
+            .build()
+    }
+
+    val api: MovieApiInterface by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(MovieApiInterface::class.java)
+    }
+}

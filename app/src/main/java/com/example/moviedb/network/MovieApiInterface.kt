633a5263138107bbbb87b399adb465f12c24cef9
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-09-27 13:46:21 +0300

    add method for getting movie details

--- app/src/main/java/com/example/moviedb/network/MovieApiInterface.kt
+++ app/src/main/java/com/example/moviedb/network/MovieApiInterface.kt
@@ -1,9 +1,14 @@
 package com.example.moviedb.network
 
+import com.example.moviedb.model.Movie
 import com.example.moviedb.model.MovieResponse
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MovieApiInterface {
     @GET("trending/movie/day")
     suspend fun getTrendingMovies(): MovieResponse
+
+    @GET("movie/{movieId}")
+    suspend fun getMovieDetails(@Path("movieId") movieId: String): Movie
 }

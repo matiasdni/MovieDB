eace89f12a342a187ebf34a9483b719681f6b63f
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-09-27 21:49:50 +0300

    use uitext util

--- app/src/main/java/com/example/moviedb/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/example/moviedb/viewmodel/MovieViewModel.kt
@@ -2,8 +2,10 @@ package com.example.moviedb.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.moviedb.R
 import com.example.moviedb.model.Movie
 import com.example.moviedb.network.ApiClient
+import com.example.moviedb.util.UiText
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -13,11 +15,11 @@ fun formatReleaseDate(releaseDate: String): String {
 }
 
 class MovieViewModel : ViewModel() {
-    private val _trendingMovies = MutableStateFlow<List<Movie>>(emptyList())
-    val trendingMovies: StateFlow<List<Movie>> get() = _trendingMovies
+    private val _popularMovies = MutableStateFlow<List<Movie>>(emptyList())
+    val popularMovies: StateFlow<List<Movie>> get() = _popularMovies
 
-    private val _loadingTrendingMovies = MutableStateFlow(true)
-    val loadingTrendingMovies: StateFlow<Boolean> get() = _loadingTrendingMovies
+    private val _loadingMovies = MutableStateFlow(false)
+    val loadingMovies: StateFlow<Boolean> get() = _loadingMovies
 
     private val _selectedMovie = MutableStateFlow<Movie?>(null)
     val selectedMovie: StateFlow<Movie?> get() = _selectedMovie
@@ -25,25 +27,39 @@ class MovieViewModel : ViewModel() {
     private val _loadingMovieDetails = MutableStateFlow(false)
     val loadingMovieDetails: StateFlow<Boolean> get() = _loadingMovieDetails
 
+    private val _errorMessage =
+        MutableStateFlow<UiText>(UiText.StringResource(R.string.empty_string))
+    val errorMessage: StateFlow<UiText> = _errorMessage
+
+    private var currentPage = 1
+    private var canLoadMore = true
+
     init {
-        fetchTrendingMovies()
+        fetchPopularMovies()
     }
 
-    private fun fetchTrendingMovies() {
+    fun fetchPopularMovies() {
+        if (_loadingMovies.value || !canLoadMore) return
+
         viewModelScope.launch {
             try {
-                _loadingTrendingMovies.value = true
-                val movieResponse = ApiClient.api.getTrendingMovies()
+                _loadingMovies.value = true
+                val movieResponse = ApiClient.api.getPopularMovies(page = currentPage)
 
-                val movies = movieResponse.movies.map { movie ->
-                    movie.copy(releaseDate = formatReleaseDate(movie.releaseDate))
+                if (movieResponse.results.isEmpty()) canLoadMore = false
+
+                val movies = movieResponse.results.map { movie: Movie ->
+                    movie.copy(release_date = formatReleaseDate(movie.release_date))
                 }
 
-                _trendingMovies.value = movies
+                _popularMovies.value += movies
+                _errorMessage.value = UiText.StringResource(resId = R.string.empty_string)
+                currentPage++
             } catch (e: Exception) {
-                _trendingMovies.value = emptyList()
+                _popularMovies.value = emptyList()
+                _errorMessage.value = UiText.StringResource(resId = R.string.error_message_movies)
             } finally {
-                _loadingTrendingMovies.value = false
+                _loadingMovies.value = false
             }
         }
     }
@@ -53,10 +69,12 @@ class MovieViewModel : ViewModel() {
             try {
                 _loadingMovieDetails.value = true
                 val response = ApiClient.api.getMovieDetails(movieId)
-                val movie = response.copy(releaseDate = formatReleaseDate(response.releaseDate))
+                val movie = response.copy(release_date = formatReleaseDate(response.release_date))
                 _selectedMovie.value = movie
+                _errorMessage.value = UiText.StringResource(resId = R.string.empty_string)
             } catch (e: Exception) {
                 _selectedMovie.value = null
+                _errorMessage.value = UiText.StringResource(resId = R.string.error_message_details)
             } finally {
                 _loadingMovieDetails.value = false
             }

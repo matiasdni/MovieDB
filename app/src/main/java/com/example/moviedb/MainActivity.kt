519b476685dc5967d163780fa4c757674426f6eb
matiasdni <99556596+matiasdni@users.noreply.github.com>
2024-09-27 13:47:06 +0300

    add navigation

--- app/src/main/java/com/example/moviedb/MainActivity.kt
+++ app/src/main/java/com/example/moviedb/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.moviedb.ui.MovieInfoScreen
 import com.example.moviedb.ui.MovieListScreen
 import com.example.moviedb.ui.theme.MovieDBTheme
 
@@ -13,8 +19,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MovieDBTheme {
-                MovieListScreen()
+                MovieApp()
             }
+        }
+    }
+}
+
+@Composable
+fun MovieApp() {
+    val navController: NavHostController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "movie_list") {
+        composable("movie_list") {
+            MovieListScreen(navController = navController)
+        }
+        composable("movie_info/{movieId}") { backStackEntry ->
+            val movieId = backStackEntry.arguments?.getString("movieId")
+            MovieInfoScreen(navController = navController, movieId = movieId)
         }
     }
 }
